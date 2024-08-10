118f935be8acf01b448881e445eb9904b580f4cf
MrSinaMaleki <sina67631@gmail.com>
2024-08-10 22:58:30 +0330

    Phaze 4

--- post/urls.py
+++ post/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, create_post, delete
+from .views import home, create_post, delete,update
 
 urlpatterns = [
     path('', home, name="home"),
     path('delete/<int:post_id>', delete, name="delete"),
+    path('update/<int:post_id>', update , name="update"),
     path('create/', create_post)
 ]
--- post/views.py
+++ post/views.py
@@ -27,9 +27,26 @@ def create_post(request):
 
 
 def delete(request, post_id):
-
     obj = get_object_or_404(Post, id=post_id)
 
     obj.delete()
     print("post was deleted!")
     return redirect('home')
+
+
+def update(request, post_id):
+
+    obj = get_object_or_404(Post, id=post_id)
+
+    form = CreatePostForm(request.POST or None, instance=obj)
+    context = dict()
+    # save the data from the form and
+    # redirect to detail_view
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+
+    # add form dictionary to context
+    context["form"] = form
+
+    return render(request, "update_view.html", context)
